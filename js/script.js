e4cc67c552cbd127c73a7f123d3ba133b0319a2a
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-03-11 19:15:18 +0100

    add script.js content

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var button = document.getElementById("button");
+
+button.addEventListener("click", function(){
+    var base = document.getElementById("triangleBase").value;
+    var height = document.getElementById("triangleHeight").value;
+    var result = document.getElementById("resultValue");
+    
+    var area = base * height / 2;
+    
+    if (area === "NaN"){
+        result.innerHTML = "Please, enter the correct data";
+    } else {
+        result.innerHTML = area + " cm <sup>2</sup>";
+    };
+    
+});
